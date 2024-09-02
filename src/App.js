bf05f89d5d0aad6c711c96cdb60be8fdd64f6911
canbbu222 <canbbu222@gmail.com>
2024-09-03 00:59:26 +0900

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,35 +1,81 @@
-import logo from './logo.svg';
-import Customer from './components/Customer'
+import React from 'react';
+import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import { withStyles } from '@mui/styles';
+import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
 
-const customers = [
-    {
-    'id' : '1',
-    'img' : 'https://source.unsplash.com/user/max_duz/300x300',
-    'name' : 'young',
-    'age' : '33',
-    'job'  : 'react engineer',
-  },
-  {
-    'id' : '2',
-    'img' : 'https://placeimg.com/64/64/any',
-    'name' : 'jisu',
-    'age' : '31',
-    'job'  : 'my girlfriend',
-  },
-  {
-    'id' : '3',
-    'img' : 'https://placeimg.com/64/64/any',
-    'name' : 'goduck',
-    'age' : '33',
-    'job'  : 'friend',
-  },
-]
+const theme = createTheme()
 
-function App() {
+const styles ={
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080,
+  },
+};
+
+const customers = [
+  {
+    id: '1',
+    img: 'https://source.unsplash.com/user/max_duz/300x300',
+    name: 'young',
+    age: '33',
+    job: 'react engineer',
+  },
+  {
+    id: '2',
+    img: 'https://placeimg.com/64/64/any',
+    name: 'jisu',
+    age: '31',
+    job: 'my girlfriend',
+  },
+  {
+    id: '3',
+    img: 'https://placeimg.com/64/64/any',
+    name: 'goduck',
+    age: '33',
+    job: 'friend',
+  },
+];
+
+function App(props) {
+  const { classes } = props;
   return (
-    customers.map(customer => {return (<Customer key = {customer.id} id = {customer.img} name = {customer.name} age = {customer.age} job = {customer.job}/>)})
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>아이디</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>나이</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(customer => (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              img={customer.img}
+              name={customer.name}
+              age={customer.age}
+              job={customer.job}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,20 @@
 import React from 'react'
+import TableRow from '@mui/material/TableRow'
+import TableCell from '@mui/material/TableCell';
 
 class Customer extends React.Component{
     render (){
         return (
-            <div>
-                <CustomerProfile img = {this.props.img} />
-                <CustomerInfo name = {this.props.name} age = {this.props.age} job = {this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell>{this.props.img}</TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.age}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component{
-    render (){
-        return(
-            <div>
-                <p>{this.props.img}</p>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render (){
-        return(
-            <div>
-                <p>{this.props.name}</p>
-                <p>{this.props.age}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer
